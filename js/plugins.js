c63f17a48c29421a8e2bbbe452ad0a3068649425
Yousef Alaa <yousefalaa11223344@gmail.com>
2020-10-14 16:00:59 +0200

    Upload Files

---
+++ js/plugins.js
@@ -0,0 +1,10 @@
+/*global $*/
+
+$(".fit-heading").fitText(1.1, { minFontSize: '33px', maxFontSize: '60px' });
+$(".fit-heading-2").fitText(1.1, { minFontSize: '27px', maxFontSize: '2rem' });
+$('.to-top').smoothScroll();
+$("body").niceScroll({
+    cursorcolor: "#0f63f9",
+    cursorwidth: "12px",
+    cursorborderradius: "6px"
+});
